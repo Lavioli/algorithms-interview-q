5e4b8ab5ae472db1e7604d85c4c4fe98dd3e8f85
LavieMaru <laviemaru@gmail.com>
2016-11-04 16:39:22 -0400

    finish question 1

--- question1-2D-array.js
+++ question1-2D-array.js
@@ -1,32 +1,48 @@
 // Write an algorithm which searches through a 2D array, and whenever it finds a zero should set the entire row and column to zero.
-/*
-2D array: [[0,1],[2,0]]
-*/
-//write a for loop to run through the rows
-//write a for loop to run through the columns
-//search through the arrays for 0. if array[i][j] = 0, turn array[i][j + 1 || j - 1] to 0 IF the index exists
-//turn array[i + 1][j] || [i + 2...][j] to 0 until i < array.length 
 var items = [
-  [0, 2],
-  [3, 4],
-  [5, 0]
+  [2, 2, 0, 6], //rowIndex = 0, columnIndex = 2
+  [3, 4, 7, 8],
+  [5, 0, 9, 1], //rowIndex = 2, columnIndex = 1
+  [7, 4, 9, 2]
 ];
 
 function twoDimArr(array, value){
-	for (var i = 0; i < array.length; i++){
-		console.log("i before:", array[i]);
-		for(var j = 0; j < array[i].length; j++){
-			console.log("j before:", array[i][j]);
-			if(array[i][j] === 0 && array[i][j + 1]){
-				array[i][j + 1] = 0;
-					console.log("j after:", array[i][j + 1]);
-			} else if (array[i][j] === 0 && array[i][j - 1]){
-				array[i][j - 1] = 0;
+	var newArr = array.slice(0);
+	//if we have more than 1 locations of 0 in the 2D array, we must push the row and column index into an array
+	var rowIndex = [0, 2];
+	var columnArray = [2, 1];
+
+	console.log('original array', array)
+
+//identify the index to change column or row
+// i represents the rows
+// j represents the columns
+	for (var i = 0; i < array.length; i++) {
+		for(var j = 0; j < array[i].length; j++) {
+			if(array[i].indexOf(0) === j) {
+				rowIndex.push(i);
+				columnArray.push(j);
 			}
-			console.log("i after:", array[i]);
 		}
 	}
+//change the row
+		for (var i = 0; i < newArr.length; i++) {
+			for (var j = 0; j < newArr[i].length; j++) {
+				for(var row = 0; row < rowIndex.length; row++) {
+					newArr[rowIndex[row]][j] = 0; 
+				}
+			}
+		}	
+//change the column
+	for (var i = 0; i < newArr.length; i++) {
+		for (var columnIndex = 0; columnIndex < columnArray.length; columnIndex++) {
+			newArr[i][columnArray[columnIndex]] = 0; 
+		}
+	}
+
+	return newArr;
+
 }
 
-
 twoDimArr(items, 0);
+
